ec72e8f07079eb3131538aeb9ef8b8e401d2d929
Lynvmili <Lynvmili@gmail.com>
2019-12-04 14:54:55 +0700

    Update login
    Update login

--- fronted/adminfe/src/reducers/authentication.js
+++ fronted/adminfe/src/reducers/authentication.js
@@ -1,5 +1,5 @@
-import {LOG_IN_ERROR, LOG_IN_SUCCESS, SET_CURRENT_USER} from "../constants/actionTypes";
-import isEmpty from 'lodash/isEmpty'
+import { SET_CURRENT_USER } from '../constants/actionTypes';
+import isEmpty from 'lodash/isEmpty';
 
 const initialState = {
     isAuthenticated: false,
@@ -7,25 +7,12 @@ const initialState = {
 };
 
 export default (state = initialState, action = {}) => {
-    switch (action.type) {
-        case SET_CURRENT_USER:
+    switch(action.type) {
+        case 'SET_CURRENT_USER':
             return {
                 isAuthenticated: !isEmpty(action.user),
                 user: action.user
             };
-        case LOG_IN_SUCCESS:
-            return {
-                isAuthenticated: true,
-                user: action.user
-            };
-        case LOG_IN_ERROR:
-            return {
-                isAuthenticated: false,
-                errors: action.errors
-            };
-        default:
-            return state
+        default: return state;
     }
 }
-
-
