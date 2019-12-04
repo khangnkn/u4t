20c21de7a851d821e0738758a782c2749efcb59a
Lynvmili <Lynvmili@gmail.com>
2019-12-04 09:18:30 +0700

    Cài đặt readux + template admin
    Cài đặt readux + template admin

---
+++ fronted/adminfe/src/components/auth/signin/SigninPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SigninForm from './SigninForm'
+
+
+class SigninPage extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <h1>Sign In</h1>
+                <SigninForm/>
+            </div>
+        )
+    }
+}
+
+export default SigninPage;
