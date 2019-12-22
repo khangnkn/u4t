35b9018b328e8b0c20bc44ab032930ff6176e52c
Lynvmili <Lynvmili@gmail.com>
2019-12-22 17:02:17 +0700

    Update chuc nang lock/unlock acount

---
+++ admin/client/src/reducers/user.reducers.js
@@ -0,0 +1,71 @@
+import * as types from '../constants/actionTypes';
+import isEmpty from 'lodash/isEmpty';
+
+const initialState = {
+    datas: [],
+    detail: {},
+    // Type: student, teacher, admin
+    role: '0',
+    paginationStudent: {
+        page: 1,
+        totalPages: 1,
+        limit: 2,
+    },
+    paginationTeacher: {
+        page: 1,
+        totalPages: 1,
+        limit: 2,
+    },
+    paginationAdmin: {
+        page: 1,
+        totalPages: 1,
+        limit: 2,
+    },
+    paginationRoot: {
+        page: 1,
+        totalPages: 1,
+        limit: 2,
+    }
+};
+
+export default (state = initialState, action = {}) => {
+    switch (action.type) {
+        case types.SET_STUDENT_LIST:
+            return {
+                datas: action.datas,
+                role: action.role,
+                detail: {},
+                paginationStudent: action.paginationStudent
+            };
+        case types.SET_TEACHER_LIST:
+            return {
+                datas: action.datas,
+                role: action.role,
+                detail: {},
+                paginationTeacher: action.paginationTeacher
+
+            };
+        case types.SET_ADMIN_LIST:
+            return {
+                datas: action.datas,
+                role: action.role,
+                detail: {},
+                paginationAdmin: action.paginationAdmin
+
+            };
+        case types.SET_ROOT_LIST:
+            return {
+                datas: action.datas,
+                role: action.role,
+                detail: {},
+                paginationRoot: action.paginationRoot
+
+            };
+        case types.SET_DETAIL_STUDENT:
+        case types.SET_DETAIL_TEACHER:
+        case types.SET_DETAIL_ADMIN:
+            return {};
+        default:
+            return state
+    }
+}
