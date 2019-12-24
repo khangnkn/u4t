06ba99a34f5e9ec2f921c0e16eb577da8d1082f9
Lynvmili <Lynvmili@gmail.com>
2019-12-24 12:15:33 +0700

    Cài đặt container cho revenue

--- admin/client/src/components/Table/RevenueTopSkill.js
+++ admin/client/src/components/Table/RevenueTopSkill.js
@@ -1,35 +1,95 @@
 import React from 'react';
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Table } from 'reactstrap';
-import Avatar from "../Avatar";
+import {Card, CardBody, CardHeader, Col, Row, Table} from 'reactstrap';
+import Input from "reactstrap/lib/Input";
+import {connect} from "react-redux";
 
 
 class RevenueTopSkill extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            value: 1
+        };
+        this.handleChange = this.handleChange.bind(this)
     }
 
+    handleChange = async (event) => {
+        try {
+            console.log(event.target.value);
+            this.setState({
+                value: event.target.value
+            })
+        } catch (e) {
+            console.log(e);
+        } finally {
+            console.log('done.');
+        }
+    };
+
     render() {
+        const items = [
+            {name: 'Today', id: 1},
+            {name: 'Week', id: 2},
+            {name: 'Month', id: 3},
+            {name: 'Year', id: 4},
+        ];
+
+        let selects = items.map((item, id) => {
+            return (
+                <option value={item.id}>{item.name}</option>
+            )
+        });
+
+        let title = `Top Revenue Skill In ${items[this.state.value - 1].name}`;
+
         return (
             <div>
-                <Table hover>
-                    <thead>
-                        <tr>
-                            <th>Skill name</th>
-                            <th>Revenue</th>
-                            <th>Growth</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>2.000.000</td>
-                            <td>30%</td>
-                        </tr>
-                    </tbody>
-                </Table>
+               <Row>
+                   <Col>
+                       <Card>
+                           <CardHeader>
+                               <Row>
+                                   <Col>
+                                       {title}
+                                   </Col>
+                                   <Col sm={3}>
+                                       <Input type="select" onChange={this.handleChange}>
+                                           {selects}
+                                       </Input>
+                                   </Col>
+                               </Row>
+                           </CardHeader>
+                           <CardBody>
+                               <Table hover>
+                                   <thead>
+                                   <tr>
+                                       <th>Skill name</th>
+                                       <th>Revenue</th>
+                                       <th>Growth</th>
+                                   </tr>
+                                   </thead>
+                                   <tbody>
+                                   <tr>
+                                       <td>Mark</td>
+                                       <td>2.000.000</td>
+                                       <td>30%</td>
+                                   </tr>
+                                   </tbody>
+                               </Table>
+                           </CardBody>
+                       </Card>
+                   </Col>
+               </Row>
             </div>
         );
     }
 };
 
-export default RevenueTopSkill;
+function mapStateToProps(state) {
+    const {revenue} = state;
+    return revenue.datasTopSkill
+}
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RevenueTopSkill);
--- admin/client/src/pages/RevenueManagementPage.js
+++ admin/client/src/pages/RevenueManagementPage.js
@@ -2,66 +2,37 @@ import React from 'react';
 import Page from '../components/Page';
 import Row from 'reactstrap/lib/Row';
 import Col from 'reactstrap/lib/Col';
-import RevenueDay from '../components/Chart/RevenueDay'
-import RevenueWeek from '../components/Chart/RevenueWeek'
-import RevenueMonth from '../components/Chart/RevenueMonth'
-import RevenueYear from '../components/Chart/RevenueYear'
+import RevenueTotal from '../components/Chart/RevenueTotal'
 import RevenueTopUser from '../components/Table/RevenueTopUser';
 import RevenueTopSkill from '../components/Table/RevenueTopSkill';
-import CardBody from 'reactstrap/lib/CardBody';
-import { Card, CardHeader } from 'reactstrap';
-
 
 
 class RevenueManagementPage extends React.Component {
     componentDidMount() {
-        // this is needed, because InfiniteCalendar forces window scroll
         window.scrollTo(0, 0);
     }
+
     render() {
         return (
             <Page
                 className="RevenueManagementPage"
                 title="Revenue"
-                breadcrumbs={[{ name: 'Revenue', active: true }]}
+                breadcrumbs={[{name: 'Revenue', active: true}]}
             >
+
                 <Row>
                     <Col lg="6" md="6" sm="12" xs="12">
-                        <RevenueDay></RevenueDay>
+                        <RevenueTopUser></RevenueTopUser>
                     </Col>
 
                     <Col lg="6" md="6" sm="12" xs="12">
-                        <RevenueWeek></RevenueWeek>
+                        <RevenueTopSkill></RevenueTopSkill>
                     </Col>
                 </Row>
 
                 <Row>
-                    <Col lg="6" md="6" sm="12" xs="12">
-                        <RevenueMonth></RevenueMonth>
-                    </Col>
-
-                    <Col lg="6" md="6" sm="12" xs="12">
-                        <RevenueYear></RevenueYear>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col lg="6" md="6" sm="12" xs="12">
-                        <Card>
-                            <CardHeader>Top revenue users</CardHeader>
-                            <CardBody>
-                                <RevenueTopUser></RevenueTopUser>
-                            </CardBody>
-                        </Card>
-                    </Col>
-
-                    <Col lg="6" md="6" sm="12" xs="12">
-                    <Card>
-                            <CardHeader>Top revenue skills</CardHeader>
-                            <CardBody>
-                            <RevenueTopSkill></RevenueTopSkill>
-                            </CardBody>
-                        </Card>
+                    <Col xs="11">
+                        <RevenueTotal></RevenueTotal>
                     </Col>
                 </Row>
             </Page>
