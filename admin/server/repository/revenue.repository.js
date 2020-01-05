c8f138063210b673101c3173ba3762dd2b9e695f
Lynvmili <Lynvmili@gmail.com>
2020-01-06 00:19:44 +0700

    Validate revenue và chức năng lấy top revenue theo user và skill

--- admin/server/repository/revenue.repository.js
+++ admin/server/repository/revenue.repository.js
@@ -1,4 +1,42 @@
-const RevenueModel = require('../shared/models/revenue.model');
+const ContractModel = require('../shared/models/contract.model');
+
+const getRevenueSortByUser = async (start, end) => {
+    const query = {
+        created_at: {$gte: start, $lt: end}
+    };
+    const sortCondition = {
+        tutor: 1
+    };
+    const res = await ContractModel
+        .find(query)
+        .populate(['tutor'])
+        .sort(sortCondition);
+
+    return {
+        res: res,
+        err: null
+    }
+};
+
+const getRevenueSortBySkill = async (start, end) => {
+    const query = {
+        created_at: {$gte: start, $lt: end}
+    };
+    const sortCondition = {
+        skill: 1
+    };
+    const res = await ContractModel
+        .find(query)
+        .populate(['skill'])
+        .sort(sortCondition);
+
+    return {
+        res: res,
+        err: null
+    }
+};
 
 module.exports = {
+    getRevenueSortByUser,
+    getRevenueSortBySkill
 };
--- admin/server/services/revenue.service.js
+++ admin/server/services/revenue.service.js
@@ -1,4 +1,107 @@
 const RevenueRepository = require('../repository/revenue.repository');
+const RESPONSE_SERVICE = require('../utils/res/service.response');
+
+const getRange = (range) => {
+    const current = new Date();
+    const year = current.getFullYear();
+    const month = current.getMonth();
+    const date = current.getDate();
+    let numDateInMonth = new Date(year, month, -1);
+
+    let start, end;
+
+    switch (range) {
+        case 'today':
+            start = new Date(year, month, date, 0, 0, 0);
+            end = new Date(year, month, date, 23, 59, 59);
+            break;
+        case 'week':
+            start = new Date(year, month, date);
+            end = new Date(year, month, date);
+            break;
+        case 'month':
+            start = new Date(year, month, 1, 0, 0, 0);
+            end = new Date(year, month, numDateInMonth.getDate() + 1, 23, 59, 59);
+            break;
+        case 'year':
+            start = new Date(year, 0, 1, 0, 0, 0);
+            end = new Date(year, 11, 31, 23, 59, 59);
+            break;
+        default:
+            start = new Date(year, month, date, 0, 0, 0);
+            end = new Date(year, month, date, 23, 59, 59);
+    }
+    return [start, end]
+};
+
+const getTopRevenueUser = async (payload) => {
+    const range = getRange(payload.range);
+    let contractList = await RevenueRepository.getRevenueSortByUser(range[0], range[1]);
+    let len = contractList.res.length;
+    let revenueTotalList = new Array(len).fill({total: 0, tutor: {}});
+    let index = -1;
+    let currentTutor = '-1';
+    contractList.res.map((contract) => {
+        if (!contract.tutor) {
+            return
+        }
+        if (!contract.tutor.equals(currentTutor)) {
+            currentTutor = contract.tutor;
+            index += 1;
+        }
+        const update = {
+            ...{
+                tutor: currentTutor,
+                total: revenueTotalList[index].total + contract.price
+            }
+        };
+        revenueTotalList[index] = update;
+    });
+
+    revenueTotalList.sort((item1, item2) => item2.total - item1.total);
+
+    const res = {
+        res: revenueTotalList,
+        err: null
+    };
+    return RESPONSE_SERVICE.serviceResponseDelete(res);
+};
+
+const getTopRevenueSkill = async (payload) => {
+    const range = getRange(payload.range);
+    let contractList = await RevenueRepository.getRevenueSortBySkill(range[0], range[1]);
+    let len = contractList.res.length;
+    let revenueTotalList = new Array(len).fill({total: 0, skill: {}});
+    let index = -1;
+    let currentSkill = '-1';
+    contractList.res.map((contract) => {
+        if (!contract.skill) {
+            return
+        }
+        if (!contract.skill.equals(currentSkill)) {
+            currentSkill = contract.skill;
+            index += 1;
+        }
+        const update = {
+            ...{
+                skill: currentSkill,
+                total: revenueTotalList[index].total + contract.price
+            }
+        };
+        revenueTotalList[index] = update;
+    });
+
+    revenueTotalList.sort((item1, item2) => item2.total - item1.total);
+
+    const res = {
+        res: revenueTotalList,
+        err: null
+    };
+    return RESPONSE_SERVICE.serviceResponseDelete(res);
+};
+
 
 module.exports = {
+    getTopRevenueUser,
+    getTopRevenueSkill
 };
