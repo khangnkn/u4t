3e3b75b15215b2086d12409ec5887f09a7a4db31
Nguyen Khac Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2020-01-08 23:11:39 +0700

    populate create message

--- user/userbe/controllers/messages.controller.js
+++ user/userbe/controllers/messages.controller.js
@@ -10,7 +10,7 @@ function parseMessage(params) {
   });
 }
 
-const CreateMessage = (req, res, next) => {
+const CreateMessage = async (req, res, next) => {
   const { body } = req;
   const err = EnsureMessage(body);
   if (err.error) {
@@ -21,33 +21,49 @@ const CreateMessage = (req, res, next) => {
     });
   }
   const msg = parseMessage(body);
-  return msg.save()
-    .then((message) => Conversation
-      .findById(body.conversation,
-        (error, conversation) => {
-          if (error) {
-            return next({
-              status: SC.INTERNAL_SERVER_ERROR,
-              code: Error.UnknownError,
-              message: 'error appending message to conversation',
-              extra: `${error}`,
-            });
-          }
-          // eslint-disable-next-line no-underscore-dangle
-          conversation.messages.push(message._id);
-          conversation.save();
-          return next({
-            status: SC.OK,
-            code: Error.Success,
-            message: 'message sent',
-            data: message,
-          });
-        })).catch((error) => next({
-      status: SC.INTERNAL_SERVER_ERROR,
-      code: Error.UnknownError,
-      message: 'failed to send message',
-      extra: `${error}`,
-    }));
+  try {
+    let message = await msg.save();
+    console.log(message);
+    const conversation = await Conversation.findById(body.conversation);
+    conversation.messages.push(message);
+    await conversation.save();
+    message = await Message.findById(message._id).populate(['sender']);
+    return next({
+      status: SC.OK,
+      code: Error.Success,
+      message: 'message sent',
+      data: message,
+    });
+  } catch (ex) {
+    return ex;
+  }
+  // return msg.save()
+  //   .then((message) => Conversation
+  //     .findById(body.conversation,
+  //       (error, conversation) => {
+  //         if (error) {
+  //           return next({
+  //             status: SC.INTERNAL_SERVER_ERROR,
+  //             code: Error.UnknownError,
+  //             message: 'error appending message to conversation',
+  //             extra: `${error}`,
+  //           });
+  //         }
+  //         // eslint-disable-next-line no-underscore-dangle
+  //         conversation.messages.push(message._id);
+  //         conversation.save();
+  //         return next({
+  //           status: SC.OK,
+  //           code: Error.Success,
+  //           message: 'message sent',
+  //           data: message,
+  //         });
+  //       })).catch((error) => next({
+  //     status: SC.INTERNAL_SERVER_ERROR,
+  //     code: Error.UnknownError,
+  //     message: 'failed to send message',
+  //     extra: `${error}`,
+  //   }));
 };
 
 module.exports = { CreateMessage };
