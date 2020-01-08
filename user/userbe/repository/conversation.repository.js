e6d4ed85adf1464b9db613f1c20d565b495c6ad0
Nguyen Khac Nguyen Khang <nguyenkhacnguyenkhang@gmail.com>
2020-01-08 22:59:29 +0700

    add populate get all conversation

--- user/userbe/repository/conversation.repository.js
+++ user/userbe/repository/conversation.repository.js
@@ -2,8 +2,9 @@ const { Conversation } = require('../models');
 
 const GetAll = async (id) => {
   try {
-    const l1 = await Conversation.find({ learner: id });
-    const l2 = await Conversation.find({ tutor: id });
+    const l1 = await Conversation.find({ learner: id }).populate(['tutor', 'learner', 'messages']);
+    const l2 = await Conversation.find({ tutor: id }).populate(['tutor', 'learner', 'messages']);
+
     return [...l1, ...l2];
   } catch (ex) {
     return ex;
